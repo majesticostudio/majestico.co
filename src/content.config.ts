e34135bfac12025557d96c1aeb7e0a8b075cefed
Giulio Zanchetta <zank@outlook.it>
2025-03-31 16:18:27 +0200

    Add optional link field to project collection
    - Introduced an optional 'link' field for projects
    - Updated schema to include new property

--- src/content.config.ts
+++ src/content.config.ts
@@ -30,6 +30,7 @@ const projectsCollection = defineCollection({
 			title: z.string(),
 			description: z.string(),
 			order: z.number(),
+			link: z.optional(z.string()),
 			createdAt: z.date(),
 			lastUpdatedAt: z.optional(z.date()),
 			hidden: z.optional(z.boolean()),
